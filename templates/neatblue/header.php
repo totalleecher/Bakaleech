8818b861395720e5634dbe1e6ca6c5245be20c70
totalleecher <60807224+totalleecher@users.noreply.github.com>
2020-05-15 01:25:34 +0630

    Update header.php

--- templates/neatblue/header.php
+++ templates/neatblue/header.php
@@ -31,4 +31,4 @@ if ($GLOBALS['options']['flist_sort']) { echo '<script src="classes/sorttable.js
 <script type="text/javascript">function toggle(b){var a=document.getElementById(b);if(a.style.display=="none"){a.style.display="block"}else{a.style.display="none"}};</script>
 </head>
 <body>
-	<header></br><img src="https://github.com/totalleecher/Bakaleech/raw/master/templates/neatblue/images/header.jpg" height="220" width="auto" alt="rapidleech neatblue by Itoshiki"></header><br />
+	<header><br><img src="https://github.com/totalleecher/Bakaleech/raw/master/templates/neatblue/images/header.jpg" height="220" width="auto" alt="rapidleech neatblue by Itoshiki"></header><br />
